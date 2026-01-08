7f57fa5899fdab16a16d04bc54ef71c3964b320e
Brittani Lovelace <blovelace84@gmail.com>
2026-01-07 19:20:00 -0500

    fixing supabase

--- online-yardsale-project/lib/supabaseClient.ts
+++ online-yardsale-project/lib/supabaseClient.ts
@@ -1,6 +1,14 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from "@supabase/supabase-js"
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
 
 export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
+  supabaseUrl,
+  supabaseAnonKey
+)
+
+console.log(
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY?.slice(0, 10)
 )
--- online-yardsale-project/next.config.ts
+++ online-yardsale-project/next.config.ts
@@ -3,7 +3,12 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["bnjqfuqlcyfhmoelfmpi.supabase.co"]
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "bnjqfuqlcyfhmoelfmpi.supabase.co",
+      },
+    ],
   },
 }
 
